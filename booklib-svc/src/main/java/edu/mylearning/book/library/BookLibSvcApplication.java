5af9d3b2cd715bd68186c9bc46dfd85a51e9be73
Sagar Grade <sagargrade@yahoo.com>
2024-06-17 13:37:59 +0200

    booklib-svc : initial commit

---
+++ booklib-svc/src/main/java/edu/mylearning/book/library/BookLibSvcApplication.java
@@ -0,0 +1,11 @@
+package edu.mylearning.book.library;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BookLibSvcApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(BookLibSvcApplication.class, args);
+    }
+}
