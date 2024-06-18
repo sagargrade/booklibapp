3c409efbb7cdb8575a1db019b12853971caabb9d
Sagar Grade <sagar.grade@colruytgroup.com>
2024-06-18 14:54:17 +0200

    booklib-svc : get books api changes

---
+++ booklib-svc/src/main/java/edu/mylearning/book/library/api/controller/impl/AbstractController.java
@@ -0,0 +1,5 @@
+package edu.mylearning.book.library.api.controller.impl;
+
+public abstract class AbstractController {
+    public static final String ROOT_CONTEXT_PATH = "${application.api.root}";
+}
--- booklib-svc/src/main/java/edu/mylearning/book/library/api/controller/impl/BookController.java
+++ booklib-svc/src/main/java/edu/mylearning/book/library/api/controller/impl/BookController.java
@@ -1,26 +1,30 @@
 package edu.mylearning.book.library.api.controller.impl;
 
-import java.util.List;
-
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
-
 import edu.mylearning.book.library.api.controller.BookApiDefinition;
 import edu.mylearning.book.library.bo.BookBO;
 import edu.mylearning.book.library.service.BookService;
 import lombok.AllArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+import static edu.mylearning.book.library.api.controller.impl.AbstractController.ROOT_CONTEXT_PATH;
+import static edu.mylearning.book.library.api.controller.impl.BookController.BASE_URL;
 
 @RestController
 @AllArgsConstructor
-@RequestMapping("/books")
-public class BookController implements BookApiDefinition {
+@RequestMapping(value = ROOT_CONTEXT_PATH + BASE_URL, produces = MediaType.APPLICATION_JSON_VALUE)
+public class BookController extends AbstractController implements BookApiDefinition {
+    public static final String BASE_URL = "/books";
 
-	private final BookService bookService;
+    private final BookService bookService;
 
-	@GetMapping
-	public List<BookBO> getBooks() {
-		return bookService.getBooks();
-	}
+    @GetMapping
+    public List<BookBO> getBooks() {
+        return bookService.getBooks();
+    }
 
 }
